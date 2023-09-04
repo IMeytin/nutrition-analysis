ff01d3caac7584cd5028e50dcb42f289f34717a5
Imeytin <irynameytin@gmail.com>
2023-09-04 14:05:08 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import icon from './icon.png';
 import './App.css';
 import Heading from './Heading';
 import { useEffect, useState } from 'react';
+import Nutrition from './Nutrition';
 
 function App() {
   const [nutrition, setNutrition] = useState("");
@@ -28,7 +29,9 @@ useEffect(() => {
     setNutrition(data)
     console.log(data)
   }
-    getNutritions();
+    if (searchValue.length !== 0) {
+      getNutritions();
+    }
 }, [searchValue]);
 
 const finalSearch = (e) => {
@@ -51,6 +54,16 @@ const handleInputInfo = (e) => {
         <button><img className='icon' src={icon} alt="search" /></button>
       </form>
 
+      {nutrition && Object.values(nutrition.totalNutrients)
+      .map ((element, index) => {
+        const {label, quantity, unit} = element;
+        return(
+          <Nutrition key={index} label = {label}
+          quantity = {quantity.toFixed(2)}
+          unit = {unit}
+          calories = {nutrition.calories}/>)
+      })
+      }
     </div>
   );
 }
--- src/Nutrition.js
+++ src/Nutrition.js
@@ -1,24 +1,21 @@
 import ingrImg from './ingr-img.jpg';
 
-const Nutrition = () => {
-  return (
-    <div className="main-container">
-      <div>
-        <img src={ingrImg} alt="ingredients" />
-      </div>
-      <div className="data-container">
-        <h2 className="data-title">NUTRITION FACTS</h2>
-        <p>calories</p>
-        <hr />
-        {/* {
-              nutrition && Object.values(nutrition.totalDaily)
-              .map ((label) => (
-                <p>{label}</p>
-              ))
-            } */}
-      </div>
-    </div>
-  );
+const Nutrition = ({label, quantity, unit, calories}) => {
+    return (
+        <div className="main-container">
+            <div>
+                <img src={ingrImg} alt="ingredients" />
+            </div>
+            <div className="data-container">
+                <h2 className="data-title">NUTRITION FACTS</h2>
+                <p>{calories} calories</p>
+                <hr />
+                <ul>
+                    <li>{label} {quantity}{unit}</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
-export default Nutrition
+export default Nutrition;
